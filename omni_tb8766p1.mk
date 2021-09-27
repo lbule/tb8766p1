c7c847f91b7986e5885e870ec1a37b2974527b02
Bule <586921@gmail.com>
2021-09-27 16:02:45 +0800

    Update omni_tb8766p1.mk

--- omni_tb8766p1.mk
+++ omni_tb8766p1.mk
@@ -14,8 +14,11 @@
 # limitations under the License.
 #
 
-# Inherit from our custom product configuration
+# Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+
+# Inherit from our custom product configuration
 $(call inherit-product, vendor/omni/config/common.mk)
 
 PRODUCT_DEVICE := tb8766p1
@@ -23,3 +26,13 @@ PRODUCT_NAME := omni_tb8766p1
 PRODUCT_BRAND := elink
 PRODUCT_MODEL := elink PAD
 PRODUCT_MANUFACTURER := elink
+
+# enable stock zip packages flash
+PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
+    ro.treble.enabled=true \
+    persist.sys.usb.config=mtp \
+    persist.service.adb.enable=1 \
+    persist.service.debuggable=1 \
+    ro.secure=1 \
+    ro.adb.secure=0 \
+    ro.allow.mock.location=0
